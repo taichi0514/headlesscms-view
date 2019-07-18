404a23049198c77fd2c675737646f3bc4d94cbc5
taichimurai <murai@tam-bourine.co.jp>
2019-07-18 13:19:01 +0900

    wip components化

---
+++ components/admin/cms-form.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="cms-form">
+    <fieldset>
+      <legend>タイトル</legend>
+      <input type="text" placeholder="タイトル">
+    </fieldset>
+
+    <fieldset>
+      <legend>本文</legend>
+      <textarea type="text" placeholder="本文"></textarea>
+    </fieldset>
+
+    <fieldset>
+      <legend>サムネイル</legend>
+      <form action="/api/upload" method="post" enctype="multipart/form-data">
+        <input type="file" name="file">
+        <button type="submit">保存</button>
+      </form>
+    </fieldset>
+
+    <fieldset>
+      <legend>OGP画像</legend>
+      <form action="/api/upload" method="post" enctype="multipart/form-data">
+        <input type="file" name="file">
+        <button type="submit">保存</button>
+      </form>
+    </fieldset>
+
+    <fieldset>
+      <legend>タグ</legend>
+      <input type="text" placeholder="タグ">
+    </fieldset>
+
+    <fieldset>
+      <legend>SEOタイトル</legend>
+      <input type="text" placeholder="SEOタイトル">
+    </fieldset>
+
+    <fieldset>
+      <legend>meta-discription</legend>
+      <textarea type="text" placeholder="meta-discription"></textarea>
+    </fieldset>
+  </div>
+</template>
+
+
+<script>
+  export default {
+  }
+</script>
+
+<style scoped lang="scss">
+  fieldset{
+    border:solid 1px rgb(223, 223, 227);
+    position: relative;
+    padding: 0;
+    margin-top: 60px;
+  }
+
+  legend {
+    color: rgb(122, 130, 145);
+    background-color: rgb(223, 223, 227);
+    padding: 0px 6px;
+    border-radius: 5px 5px 0 0;
+    position: absolute;
+    top: 0;
+    left: 0;
+    transform: translateX(-1%) translateY(-100%);
+    font-weight: bold;
+  }
+
+  input {
+    border-radius: 5px 5px 0 0;
+    padding: 16px 20px;
+    width: 100%;
+    border:none ;
+    font-size: 100%;
+  }
+
+  textarea{
+    border-radius: 5px 5px 0 0;
+    padding: 16px 20px;
+    width: 100%;
+    border:none ;
+    font-size: 100%;
+  }
+</style>
--- pages/admin.vue
+++ pages/admin.vue
@@ -1,57 +1,18 @@
 <template>
   <div class="admin">
-    <div class="cms-form">
-      <fieldset>
-        <legend>タイトル</legend>
-        <input type="text" placeholder="タイトル">
-      </fieldset>
-
-      <fieldset>
-        <legend>本文</legend>
-        <textarea type="text" placeholder="本文"></textarea>
-      </fieldset>
-
-      <fieldset>
-        <legend>サムネイル</legend>
-        <form action="/api/upload" method="post" enctype="multipart/form-data">
-          <input type="file" name="file">
-          <button type="submit">保存</button>
-        </form>
-      </fieldset>
-
-      <fieldset>
-        <legend>OGP画像</legend>
-        <form action="/api/upload" method="post" enctype="multipart/form-data">
-          <input type="file" name="file">
-          <button type="submit">保存</button>
-        </form>
-      </fieldset>
-
-      <fieldset>
-        <legend>タグ</legend>
-        <input type="text" placeholder="タグ">
-      </fieldset>
-
-      <fieldset>
-        <legend>SEOタイトル</legend>
-        <input type="text" placeholder="SEOタイトル">
-      </fieldset>
-
-      <fieldset>
-        <legend>meta-discription</legend>
-        <textarea type="text" placeholder="meta-discription"></textarea>
-      </fieldset>
-    </div>
-
+    <CmsForm></CmsForm>
     <div class="cms-form-view">
-
     </div>
   </div>
 </template>
 
 <script>
-
-  export default {}
+  import CmsForm from '~/components/admin/cms-form.vue'
+  export default {
+    components:{
+      CmsForm
+    }
+  }
 </script>
 
 <style scoped lang="scss">
@@ -73,38 +34,5 @@
     padding: 40px 20px;
   }
 
-  fieldset{
-    border:solid 1px rgb(223, 223, 227);
-    position: relative;
-    padding: 0;
-    margin-top: 60px;
-  }
 
-  legend {
-    color: rgb(122, 130, 145);
-    background-color: rgb(223, 223, 227);
-    padding: 0px 6px;
-    border-radius: 5px 5px 0 0;
-    position: absolute;
-    top: 0;
-    left: 0;
-    transform: translateX(-1%) translateY(-100%);
-    font-weight: bold;
-  }
-
-  input {
-    border-radius: 5px 5px 0 0;
-    padding: 16px 20px;
-    width: 100%;
-    border:none ;
-    font-size: 100%;
-  }
-
-  textarea{
-    border-radius: 5px 5px 0 0;
-    padding: 16px 20px;
-    width: 100%;
-    border:none ;
-    font-size: 100%;
-  }
 </style>
