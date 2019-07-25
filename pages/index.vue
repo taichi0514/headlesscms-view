6cbfa20c3ad46f367ac1c5879f2debd7d941d30d
taichimurai <murai@tam-bourine.co.jp>
2019-07-25 12:00:50 +0900

    fix index.vue 仮リンク

--- pages/index.vue
+++ pages/index.vue
@@ -2,23 +2,8 @@
   <section class="container">
     <div>
       <logo />
-      <h1 class="title">
-        headlesscms-view
-      </h1>
-      <h2 class="subtitle">
-        My incredible Nuxt.js project
-      </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
+        <a href="admin" class="button--green">CMS管理画面</a>
       </div>
     </div>
   </section>
