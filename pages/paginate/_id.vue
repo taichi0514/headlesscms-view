6259182589bc7ffa127aa930f8fea5577a9928cd
taichimurai <murai@tam-bourine.co.jp>
2019-08-07 10:57:13 +0900

    WIP paginate

---
+++ pages/paginate/_id.vue
@@ -0,0 +1,258 @@
+<template>
+  <div class="article_area">
+    <div class="article_area_bg"></div>
+    <div class="article_list">
+      <article class="article_list_item active" v-for="(item,index) in ArticleList" :key="_page">
+        <a class="article_list_item_link" :href=" 'article/' +item.id">
+          <img v-if="item.featured_image　=== null" src="//placehold.jp/290x160.png" alt=""
+               class="article_list_item_image">
+          <img v-else :src="item.featured_image" alt="" class="article_list_item_image">
+          <section class="article_list_item_section">
+            <h3 class="article_list_item_section_title">{{item.title}}</h3>
+            <p class="article_list_item_section_text">{{item.meta_description}}</p>
+            <a :href="'category/' + item.tag" class="article_list_item_category_link">
+              <span class="article_list_item_category">{{item.tag}}</span>
+              <p>{{current_page_next}}</p>
+            </a>
+          </section>
+        </a>
+      </article>
+    </div>
+    <a :href="ho" class="article_list_link">みぎ</a>
+    <button class="article_list_link">戻る</button>
+    <button class="article_list_link" @click="next()">次へ</button>
+
+  </div>
+</template>
+
+
+<script>
+  export default {
+    data() {
+      return {
+        ArticleList: {
+          id: '',
+          title: '',
+          body: '',
+          featured_image: '',
+          tag: '',
+          seo_title: '',
+          meta_description: '',
+        },
+        current_page: this.$route.params.id,
+      }
+    },
+
+    computed: {
+      current_page_next: function(){
+        this.current_page
+      }
+    },
+
+    created() {
+      console.log("hoge" + this.current_page_next)
+      const params = {
+        params: {
+          // ここにクエリパラメータを指定する
+          paginate: 4,
+          page: this.$route.params.id
+        }
+      };
+      this.$axios.$get(process.env.API + 'paginate', params)
+        .then(res => {
+          let resdata = res.data.map(res => {
+            return res
+          })
+          this.ArticleList = resdata
+        })
+    },
+    methods: {
+      next: function () {
+        const params = {
+          params: {
+            paginate: 4,
+            page: this.$route.params.id++
+          }
+        };
+        this.$axios.$get(process.env.API + 'paginate', params)
+          .then(res => {
+            this.current_page = res.current_page
+            let resdata = res.data.map(res => {
+              return res
+            })
+            this.ArticleList = resdata
+          })
+      },
+      return: function () {
+        const params = {
+          params: {
+            paginate: 4,
+            page: this.$route.params.id--
+          }
+        };
+        this.$axios.$get(process.env.API + 'paginate', params)
+          .then(res => {
+            this.current_page = res.current_page
+            let resdata = res.data.map(res => {
+              return res
+            })
+            this.ArticleList = resdata
+          })
+      }
+    },
+  }
+</script>
+
+
+<style lang="scss" scoped>
+
+  .article_area {
+    padding: 0 0 40px;
+    @include mq() {
+      padding: 100px 0;
+    }
+  }
+
+  .article_area_bg {
+    background-color: #F4F4F4;
+    display: block;
+    width: 100%;
+    max-width: 1100px;
+    height: 280px;
+    margin: 0 auto;
+    position: relative;
+    left: 50%;
+    transform: translateX(-50%);
+    @include mq() {
+      background-color: transparent;
+      width: 110%;
+      max-width: 1600px;
+    }
+  }
+
+  .article_list {
+    max-width: 1235px;
+    width: 100%;
+    margin: 0 auto;
+    transform: translateY(-20%);
+    display: flex;
+    padding: 40px 25px;
+    flex-direction: column;
+    align-items: center;
+    background-color: #F4F4F4;
+    @include mq(llg) {
+      background-color: transparent;
+      padding: 0;
+      flex-direction: row;
+      transform: translateY(-52%);
+    }
+  }
+
+  .article_list_item {
+    border: #CCCCCC 1px solid;
+    max-width: 290px;
+    width: 100%;
+    list-style: none;
+    background-color: #fff;
+    margin-top: 15px;
+    margin-bottom: -40px;
+    opacity: 0;
+    transition: margin-bottom 1s, opacity 2.5s;
+    margin-left: 0;
+    overflow: hidden;
+    @include mq(llg) {
+      width: 30%;
+      height: 390px;
+      margin-top: 0;
+      margin-left: 2%;
+    }
+  }
+
+  .article_list_item_image {
+    transition: 1s transform;
+    height: 160px;
+  }
+
+  .article_list_item:hover .article_list_item_image {
+    transform: scale(1.1);
+  }
+
+  .article_list_item:first-child {
+    margin-left: 0;
+  }
+
+  .article_list_item.active {
+    margin-bottom: 0;
+    opacity: 1;
+  }
+
+  .article_list_item_image {
+    width: 100%;
+  }
+
+  .article_list_item_section {
+    padding: 20px;
+  }
+
+  .article_list_item_section_title {
+    margin: 0;
+    font-weight: 700;
+    @include fontsize(16);
+  }
+
+  .article_list_item_section_text {
+    margin-top: 4px;
+    margin-bottom: 0;
+    font-weight: 300;
+  }
+
+  .article_list_item_category {
+    margin-top: 20px;
+    display: inline-block;
+    border: #F4F4F4 solid 1px;
+    border-radius: 16px;
+    padding: 4px 16px;
+    box-sizing: border-box;
+    @include fontsize(12);
+    color: #000;
+    text-align: left;
+  }
+
+  .article_list_item_category_link {
+    text-decoration: none;
+  }
+
+  .article_list_item_link {
+    text-decoration: none;
+    color: #000;
+  }
+
+
+  .article_list_link {
+    @include index-link-btn();
+    margin: -285px auto 0;
+    display: block;
+    padding: 12px 0;
+    max-width: 260px;
+    @include mq {
+      max-width: 240px;
+      padding: 12px 0px;
+      margin: -140px auto 0;
+    }
+
+  }
+
+  .article_list_link.link_btn::after {
+    transform: translateX(-160%) translateY(-50%);
+    transition: 1s transform;
+  }
+
+  .article_list_link:hover::after {
+    transform: translateX(-60%) translateY(-50%);
+  }
+
+
+</style>
+
+
+
